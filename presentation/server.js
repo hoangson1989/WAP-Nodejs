06e4141d9295b1be3bfc69d94d85d47f043229aa
Qbuiba <37890918+Qbuiba@users.noreply.github.com>
2024-03-16 10:50:46 -0500

    send cookie username

--- presentation/server.js
+++ presentation/server.js
@@ -27,23 +27,23 @@ app.listen(80, () => {
 });
 
 //
-app.get("/",(req,res)=>{
-    let user = req.cookies.user
-    if (user == undefined || user == null) {
-        res.redirect('/login')
-    } else {
-        res.redirect('/player')
-    }
-})
-app.get('/login',function(req, res){
-    res.render('login')
-})
-app.get('/player',function(req, res){
-    res.render('player')
-})
-app.get('/game',function(req, res){
-    res.render('game')
-})
+app.get("/", (req, res) => {
+	let user = req.cookies.user;
+	if (user == undefined || user == null) {
+		res.redirect("/login");
+	} else {
+		res.redirect("/player");
+	}
+});
+app.get("/login", function (req, res) {
+	res.render("login");
+});
+app.get("/player", function (req, res) {
+	res.render("player");
+});
+app.get("/game", function (req, res) {
+	res.render("game");
+});
 
 //
 app.post("/login", function (req, res) {
@@ -62,21 +62,21 @@ app.post("/login", function (req, res) {
 	} else {
 		let user = userManager.loginUser(username, password);
 		if (user != null) {
-			res.cookie("user", user);
+			res.cookie("username", user.username);
 			// Check if the user is an admin
-            if (user.type === "admin") {
-                res.redirect("/admin"); // Redirect to admin page
-            } else {
-                res.redirect("/"); // Redirect to regular user page
-            }
+			if (user.type === "admin") {
+				res.redirect("/admin"); // Redirect to admin page
+			} else {
+				res.redirect("/profile"); // Redirect to regular user page
+			}
 		} else {
 			res.send("User doesn't exist");
 		}
 	}
 });
 
-app.get(`/ranking`,(req,res)=>{
-	res.render(`ranking`)
+app.get(`/ranking`, (req, res) => {
+	res.render(`ranking`);
 });
 
 app.use(`/admin`, adminRouter);
